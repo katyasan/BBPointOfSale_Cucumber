745a9cef0aee008eb56d1dc9decbaa528ef8548c
Hleb <12hleb@gmail.com>
2020-02-27 17:48:07 -0600

    add hooks, gitignore, and my bills feature file

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "step_definition",
         dryRun = false,
-        tags = "@"
+        tags = "@Test4"
 
 )
 
