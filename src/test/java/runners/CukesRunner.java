3911f88acecab865b8638ae3db54247605e6543e
m45ulker <bicer77@hotmail.com>
2020-03-02 08:47:32 -0600

    this is my codes for PosBox/Hardware

---
+++ src/test/java/runners/CukesRunner.java
@@ -0,0 +1,17 @@
+package runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty","json:target/cucumber-reports/Cucumber.json"},
+        features = "src/test/resources/features",
+        glue = "step_definition",
+        dryRun = false,
+        tags = "@testAll"
+)
+
+public class CukesRunner {
+}
